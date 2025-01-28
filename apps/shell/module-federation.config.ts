f3c67292aed73cc8227dc3587675e63ec2f15e58
nitsan <nitsan@next-insurance.com>
2025-01-28 15:40:05 +0200

    add questionnaire app

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['lead-form'],
+  remotes: ['lead-form', 'questionnaire'],
 };
 
 export default config;
--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'questionnaire',
+    loadChildren: () =>
+      import('questionnaire/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: 'lead-form',
     loadChildren: () => import('lead-form/Routes').then((m) => m.remoteRoutes),
   },
